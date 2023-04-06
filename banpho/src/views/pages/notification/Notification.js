ab241423968a5d27dc8fa6bb528b5f17c622fec5
Gorrof <Nuttunyapong@hotmail.com>
2023-04-06 13:49:53 +0700

    update reload

--- banpho/src/views/pages/notification/Notification.js
+++ banpho/src/views/pages/notification/Notification.js
@@ -83,7 +83,7 @@ const Notification = () => {
             })
             .then(function (response) {
                 if (role == 1) {
-                    window.location.href = 'http://localhost:3000/documents';
+                    window.location.reload();
                 } else {
                     window.location.href = 'http://localhost:3000/report-documents';
                 }
